2098d5c91456433fb634e66ddb65fdb679fee72c
Marble178NewAc <97432282+Marble178NewAc@users.noreply.github.com>
2022-01-24 14:13:10 +0000

    Example
    Its only the example, you can play around with this random thing, and we will do dice but uses number from users input

---
+++ example.py
@@ -0,0 +1,15 @@
+import random
+# type 1
+a = random.randint(1,6)
+print(a)
+# type 2
+b = random.randint(1,6)
+c = random.randint(1,6)
+print(b+c)
+# type 3
+d = random.randrange(6)
+print(d)
+# type 4
+e = random.randrange(6)
+f = random.randrange(6)
+print(e+f)
